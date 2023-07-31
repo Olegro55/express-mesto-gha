4634a1dadd9a82aaffaab97b9ac04d608868cf3f
Olegro55 <120279592+Olegro55@users.noreply.github.com>
2023-07-31 10:36:39 +0000

    errors

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 
 const userRouter = require('./user');
 const cardRouter = require('./card');
-const responseCodes = require('../utils/constants');
+const { NotFoundError } = require('../errors');
 
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
-router.use('*', (_, res) => { res.status(responseCodes.NOT_FOUND).send({ message: 'Страница не найдена' }); });
+router.use('*', (req, res, next) => { next(new NotFoundError('Страница не найдена')); });
 
 module.exports = router;
