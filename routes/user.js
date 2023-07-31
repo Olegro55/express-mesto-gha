4b30afa689cfbe9e8623b93a05e5d9aeaa679c11
Olegro55 <120279592+Olegro55@users.noreply.github.com>
2023-07-31 14:39:21 +0000

    users validation

--- routes/user.js
+++ routes/user.js
@@ -18,7 +18,16 @@ router.get('/:userId', celebrate({
   }),
 }), getUser);
 
-router.patch('/me', updateUser);
-router.patch('/me/avatar', updateAvatar);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateUser);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().regex((/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/)),
+  }),
+}), updateAvatar);
 
 module.exports = router;
